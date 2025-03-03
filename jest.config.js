2f1127260cfeea0a49a7ee08c6dd9f3ec9d287b1
Santhosh Urs <santhoshurs@Mac.lan>
2025-03-03 23:02:42 +0000

    Added test reporting

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,12 @@
-
 export default {
     testEnvironment: 'node',
-    //testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
     testMatch: ['**/tests/*.spec.js'],
-    // transform: {
-    //     '^.+\\.jsx?$': 'babel-jest',
-    // },
     transform: {},
     moduleFileExtensions: ['js', 'jsx', 'json', 'node','mjs', 'cjs'],
-    testSequencer: './testSequencer.cjs' // Since consumer side tests needs to be run before provider side tests
+    testSequencer: './testSequencer.cjs', // Since consumer side tests needs to be run before provider side tests
+    reporters: [
+        "default",
+        ["jest-junit", { outputDirectory: "results/jest", outputName: "junit.xml" }],
+        ["jest-html-reporters", { publicPath: "./results/jest", filename: "test-report.html", expand: true }],
+      ]
 };
